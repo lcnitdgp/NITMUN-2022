a18234d5313720cee51e9601eb4f16d1fb6a4b98
Richa Roy <richaroydps@gmail.com>
2022-12-30 21:05:26 +0530

    backebd

---
+++ models/Payment.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose")
+
+const paymentSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true,
+    },
+    amount:{
+        type: Number,
+        required: true,
+    },
+    paidto:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        max: 50,
+    },
+    phoneNumber:{
+        type: Number,
+        required: true,
+        min: 10,
+    },
+    institute:{
+        type: String, 
+        required: true,
+    },
+    committeeAlloted: {
+        type: String,
+        default: "",
+       },
+    portfolioAlloted:{
+        type: String,
+        default: "",  
+       },
+    Date:{
+        type: String,
+        default: ""
+       },
+       preference1:{
+        type: String,
+        required: true,
+    },
+    preference2:{
+        type: String,
+        required: true,
+    },
+    preference3:{
+        type: String,
+        required: true,
+    },
+}, {timestamps: true})
+
+module.exports = mongoose.model('Payment', paymentSchema)
---
+++ routes/auth.js
@@ -0,0 +1,114 @@
+const express = require ('express');
+const Admin = require('../models/Admin');
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+const {JWT_SECRET}=require('../keys')
+
+
+// router.get('/',(req,res)=>{
+//     console.log('ok');
+//     res.send('hello')
+// })
+
+const maxAge = 3 * 24 * 3600;
+const createtokens = (id) => {
+    return jwt.sign({ id }, JWT_SECRET, {
+        expiresIn: maxAge
+    })
+}
+
+
+
+router.post("/signup", async (req, res) => {
+    try {
+
+       
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        const newAdmin = new Admin({
+            username: req.body.username,
+            password: hashedPassword,
+        });
+        const admin = await newAdmin.save();
+
+        console.log(admin);
+        const token = createtokens(admin._id)
+        res.cookie('nitmun', token, { httpOnly: true, maxAge: maxAge * 1000 })
+        res.send(admin)
+    } catch (err) {
+        res.json(err);
+    }
+
+})
+
+
+// router.post('/',async(req,res)=>{
+//     const {username,password} = req.body;
+    
+     
+
+// })
+router.post("/",(req,res)=>{
+
+    const {username,password}= req.body;
+
+    if(!username||!password){
+         return res.status(422).json({error:"add username or password"})
+    }
+    Admin.findOne({username:username})
+    .then(savedAdmin=>{
+        if(!savedAdmin){
+            return res.status(422).json({error:"Invalid username or password"})
+        }
+        bcrypt.compare(password,savedAdmin.password)
+        .then(doMatch=>{
+            if(doMatch){
+                
+                // const token = jwt.sign({_id:savedAdmin._id},JWT_SECRET);
+               
+                const token = createtokens(savedAdmin._id)
+                res.json({token})
+    
+            res.cookie('nitmun',token,{httpOnly: true, maxAge: maxAge * 1000})
+            // res.status(201).render('home')
+
+            }
+            else{
+                return res.status(422).json({error:"Invalid username or password"})
+            }
+        }).catch(err=>{
+            console.log(err)
+        })
+    })
+})
+
+
+// router.post("/", async (req, res) => {
+//     const {username,password} = req.body
+//     try {
+//         const user = await Admin.login(username,password)
+//         const token = createtokens(user._id)
+    
+//             res.cookie('nitmun',token,{httpOnly: true, maxAge: maxAge * 1000})
+//             res.status(201).render('home')
+        
+//     } catch (err) {
+//         console.log(err);
+//         res.status(400).json({error: err})
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
